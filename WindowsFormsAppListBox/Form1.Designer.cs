7d13a801fdc02d68b1aa5fed9a4e8423f770683b
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2021-01-05 20:27:14 +0100

    Add project files.

---
+++ WindowsFormsAppListBox/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WindowsFormsAppListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 20);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(800, 430);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Size";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Date";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ WindowsFormsAppListBox/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppListBox
+{
+    public partial class Form1 : Form
+    {
+        private readonly ImageList smallImageList = new ImageList();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // We will use this as a cache that we build over time
+            listView1.SmallImageList = smallImageList;
+        }
+
+        private void Form1_Shown( object sender, EventArgs e )
+        {
+            PostUpdate( Environment.GetFolderPath( Environment.SpecialFolder.MyDocuments ) );
+        }
+
+        private void PostUpdate( string path )
+        {
+            if ( listView1.InvokeRequired )
+            {
+                MessageBox.Show( "DEV: Call this on event thread" );
+                return;
+            }
+
+            listView1.Items.Clear();
+
+            Task.Run( () =>
+            {
+                Update( path );
+            } );
+        }
+
+        private void Update( string path )
+        {
+            try
+            {
+                var dirs = new List<DirectoryInfo>();
+                foreach ( var dir in Directory.GetDirectories( path ) )
+                {
+                    dirs.Add( new DirectoryInfo( dir ) );
+                }
+
+                var files = new List<FileInfo>();
+                foreach ( var file in Directory.GetFiles( path ) )
+                {
+                    files.Add( new FileInfo( file ) );
+                }
+
+                Display( path, dirs, files );
+            }
+            catch ( Exception ex )
+            {
+                MessageBox.Show( $"Failed: {ex.Message}" );
+            }
+        }
+
+        private void Display( string path, List<DirectoryInfo> dirs, List<FileInfo> files )
+        {
+            if ( listView1.InvokeRequired )
+            {
+                listView1.Invoke( (MethodInvoker) delegate 
+                {
+                    Display( path, dirs, files );
+                } );
+                return;
+            }
+
+            try
+            {
+                textBox1.Text = path;
+                listView1.Tag = path;
+
+                listView1.BeginUpdate();
+
+                foreach ( var dir in dirs )
+                {
+                    var lvi = new ListViewItem( dir.Name );
+
+                    if ( !smallImageList.Images.ContainsKey( dir.FullName ) )
+                    {
+                        smallImageList.Images.Add( dir.FullName, Icons.GetSmallIcon( dir.FullName ) );
+                    }
+                    lvi.ImageKey = dir.FullName;
+                    lvi.Tag = dir;
+
+                    listView1.Items.Add( lvi );
+                }
+
+                foreach ( var file in files )
+                {
+                    var lvi = new ListViewItem( file.Name );
+
+                    if ( !smallImageList.Images.ContainsKey( file.FullName ) )
+                    {
+                        smallImageList.Images.Add( file.FullName, Icons.GetSmallIcon( file.FullName ) );
+                    }
+                    lvi.ImageKey = file.FullName;
+                    lvi.Tag = file;
+
+                    listView1.Items.Add( lvi );
+                }
+            }
+            finally 
+            { 
+                listView1.EndUpdate();
+            }
+        }
+
+        private void listView1_KeyDown( object sender, KeyEventArgs e )
+        {
+            if ( e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return || e.KeyCode == Keys.Right )
+            {
+                if ( listView1.SelectedItems.Count > 0 )
+                {
+                    var lvi = listView1.SelectedItems[ 0 ];
+                    if ( lvi.Tag is DirectoryInfo )
+                    {
+                        PostUpdate( ( lvi.Tag as DirectoryInfo ).FullName );
+                    }
+                }
+            }
+            else if ( e.KeyCode == Keys.Back || e.KeyCode == Keys.Left )
+            {
+                var path = listView1.Tag as string;
+                PostUpdate( Directory.GetParent( path ).FullName );
+            }
+        }
+
+        private void textBox1_KeyDown( object sender, KeyEventArgs e )
+        {
+            if ( e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return )
+            {
+                if ( textBox1.Text.Length > 0 )
+                {
+                    PostUpdate( textBox1.Text );
+                }
+            }
+        }
+    }
+}
